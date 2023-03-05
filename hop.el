0ec38d4b5c8fcd945b27103c9b1a94083976a52c
Animesh Sahu <animeshsahu19@yahoo.com>
2023-03-06 03:55:40 +0530

    Initiate project with metadata

---
+++ hop.el
@@ -0,0 +1,25 @@
+;;; hop.el --- An easymotion-like plugin highly inspired from hop.nvim
+
+;; Author: Animesh Sahu <animeshsahu19@yahoo.com>
+;; URL: https://github.com/Animeshz/hop.el
+;; Version: 0.1
+;; Package-Requires: ((emacs "28.0"))
+
+
+;;; Commentary:
+
+;; Emacs Motion on Speed!
+;; Move anywhere in your buffer with 1 or 2 characters.
+;;
+;; Defines following (TODO: tentative)
+;; hop-word
+;; hop-char
+;; hop-line
+;; hop-line-skip-whitespace
+;; hop-pattern (maybe?)
+
+
+;;; Code:
+
+(provide 'hop)
+;;; my-package.el ends here
